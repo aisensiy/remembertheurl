fbf13fc07faa49561d83160b1b2a2515599c3a0e
aisensiy <aisensiy@gmail.com>
2015-02-14 22:25:43 +0800

    init stratergy for url view record

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,47 @@
 'use strict';
 
+var Api = (function() {
+  function save_views(views, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:3000/api/create', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState != 4 || xhr.status < 200) {
+        return;
+      }
+      callback && callback();
+    };
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send('data=' + encodeURIComponent(JSON.stringify(views)));
+  }
+  return {
+    save_views: save_views
+  };
+})();
+
+var db = [];
+var max_length = 50;
+
+function upload_data() {
+  var copy_db = db.slice();
+  db = [];
+  Api.save_views(copy_db);
+}
+
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    db.push(request);
+    if (db.length >= max_length) {
+      upload_data();
+    }
+    sendResponse(200);
+  });
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+setInterval(function() {
+  if (db.length) {
+    upload_data();
+  }
+}, 1000 * 60 * 10);
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,71 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+var last_action_ts = +new Date();
+var interval = 1000 * 30;
+var no_action_timer;
+
+var Message = (function() {
+  var view_obj;
+  function create_view_object() {
+    console.log('active view');
+    if (view_obj) {
+      return;
+    }
+
+    view_obj = {
+      title: document.title,
+      url: location.href,
+      start_ts: +new Date()
+    };
+  }
+  function close_view_object() {
+    console.log('close view');
+    view_obj.duration = last_action_ts - view_obj.start_ts;
+    send_message(function() {
+      view_obj = null;
+      clearTimeout(no_action_timer);
+      no_action_timer = null;
+    });
+  }
+  function send_message(callback) {
+    chrome.runtime.sendMessage(view_obj, function(response) {
+      console.log(200);
+      callback && callback();
+    });
+  }
+  return {
+    active_view: create_view_object,
+    close_view: close_view_object,
+    send_message: send_message
+  };
+})();
+
+no_action_timer = setTimeout(function() {
+  Message.close_view();
+}, interval);
+
+var has_action = function() {
+  var now = +new Date();
+  if (now - last_action_ts > 499 || !no_action_timer) {
+    Message.active_view();
+    if (no_action_timer) {
+      clearTimeout(no_action_timer);
+    }
+    no_action_timer = setTimeout(function() {
+      Message.close_view();
+    }, interval);
+    last_action_ts = now;
+  }
+};
+
+window.addEventListener('mousemove', has_action);
+window.addEventListener('keydown', has_action);
+window.addEventListener('scroll', has_action);
+window.addEventListener('focus', function() {
+  Message.active_view();
+});
+window.addEventListener('beforeunload', function() {
+  Message.close_view();
+});
+
+Message.active_view();
